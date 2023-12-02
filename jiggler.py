2cde1a524b1c3be4eea2d6ed55c9250d8610c8ce
bvillano05 <bvillano05@gmail.com>
2023-12-02 17:58:45 -0500

    Functional mouse jiggler
    Basic implementation of the jiggler program

--- jiggler.py
+++ jiggler.py
@@ -1,14 +1,20 @@
 import pyautogui
 import time
 
+SECONDS = 10 #variable with how many seconds between executing the program (edit this to change how often it jiggles)
 
-def timer(seconds=10):
-    pass
+def jiggle():
+    originalMouseX, originalMouseY = pyautogui.position() # Gets current mouse position
+    pyautogui.moveTo(originalMouseX+10,originalMouseY)
+    time.sleep(0.2) #waits
+    pyautogui.moveTo(originalMouseX,originalMouseY)
 
-
+    
 
 def main():
-    pass
-
+    while True: #Program runs until manually stopped
+        jiggle()
+        time.sleep(SECONDS) #Waits for SECONDS constant amount of seconds before doing next iteration of loop
+        
 if __name__ == "__main__":
     main()
